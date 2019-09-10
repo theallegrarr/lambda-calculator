a8f8c9073790553e9cc5738434df4f881c416853
Nurudeen <nurudeen.amedu@gmail.com>
2019-09-10 13:54:08 +0100

    Numbers component completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./App.css";
 // STEP 4 - import the button and display components
-// Don't forget to import any extra css/scss files you build into the correct component
+// Don't forget to import any extra css/scss files you build into 
+// the correct component
+import ButtonComponents from './components/ButtonComponents/NumberButtons/Numbers';
+import DisplayComponents from './components/DisplayComponents/Display';
+import OperatorButtons from './components/ButtonComponents/OperatorButtons/Operators';
+import SpecialButtons from './components/ButtonComponents/SpecialButtons/Specials';
+
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import OperatorButton from "./components/ButtonComponents/OperatorButtons/OperatorButton";
+import SpecialButton from "./components/ButtonComponents/SpecialButtons/SpecialButton";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -17,7 +25,19 @@ function App() {
     <div className="container">
       <Logo />
       <div className="App">
-        {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        {/* STEP 4 - Render your components here and be sure to properly import/export all files */
+          
+        }
+        <DisplayComponents />
+      </div>
+      <div className="buttons">
+          <div className="specials">
+            <SpecialButtons />
+            <ButtonComponents />
+          </div>
+        <div className="operators">
+          <OperatorButtons />
+        </div>
       </div>
     </div>
   );
